3554db89cb9420a9d3f2407e14ce95237faeca2d
linuxrafiq <rafiq.i@live.co.uk>
2020-04-19 13:45:38 +0600

    installed csruf token package by command: npm install csurf --save . used this csruf in routes index.js file and pass it to ui signup.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var mongoUtil = require( '../utils/mongoUtil' );
 var router = express.Router();
+var csrf = require('csurf');
 
+var csrfProtection = csrf();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var db = mongoUtil.getDb();
@@ -18,4 +20,7 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.get('/user/signup', function(req, res, next){
+  res.render(user/signup, {csrfToken:req.csrfToken});
+});
 module.exports = router;
