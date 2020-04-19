fde3feb446740d9baec082610e26584ca200f2d6
linuxrafiq <rafiq.i@live.co.uk>
2020-04-19 12:14:07 +0600

    user model created

---
+++ models/user.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    email:{type: String, required:true},
+    password:{type: String, required:true}
+});
+
+module.exports = mongoose.model('User', schema);
